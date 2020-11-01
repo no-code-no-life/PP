46bdb1ad4308fca50ee12c70a13d56ff5caca06a
ODIUM <desolh1z1@gmail.com>
2020-11-01 22:52:35 +0200

    second commit

--- main.py
+++ main.py
@@ -8,12 +8,11 @@ def hello():
     return "hello"
 
 
-@app.route("/api/v1/hello-worldapi/v1/hello-world-<int:post_id>")
+@app.route("/api/v1/hello-world-<int:post_id>")
 def hello_world(post_id):
     return "Hello World! %d" % post_id
 
 
 if __name__ == "__main__":
     app.run()
-
 #      gunicorn --bind 0.0.0.0:5000 main:app
