50c0513a3bad0fbecbc509cca158a86a132dc5ff
ODIUM <desolh1z1@gmail.com>
2020-11-01 19:45:08 +0200

    project_commit

---
+++ main.py
@@ -0,0 +1,19 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello():
+    return "hello"
+
+
+@app.route("/api/v1/hello-worldapi/v1/hello-world-<int:post_id>")
+def hello_world(post_id):
+    return "Hello World! %d" % post_id
+
+
+if __name__ == "__main__":
+    app.run()
+
+#      gunicorn --bind 0.0.0.0:5000 main:app
